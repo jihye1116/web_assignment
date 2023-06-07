d11563caf0a81ad6d16bb9b54a9555694bf66006
Wickhye <jihyesim170@gmail.com>
2023-06-07 13:04:16 +0900

    SET :: firebase

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import { fireStore } from "./firebase";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  useEffect(() => {
+    console.log(fireStore);
+  });
+
+  return <div className="App">d</div>;
 }
 
 export default App;
---
+++ src/firebase.js
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDhyRkJ6lrXUh2Iio6YEr5BAeWzlbF1YF4",
+  authDomain: "asdf-6dfbb.firebaseapp.com",
+  projectId: "asdf-6dfbb",
+  storageBucket: "asdf-6dfbb.appspot.com",
+  messagingSenderId: "674122100969",
+  appId: "1:674122100969:web:bfa26052a332bc62dac078",
+  measurementId: "G-6VVFT909WR",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const fireStore = getAnalytics(app);
+const authService = getAuth(app);
+
+export { fireStore };
