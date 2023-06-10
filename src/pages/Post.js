77f2ee8fa7e501c7fd880b107533946d7469a68c
Wickhye <jihyesim170@gmail.com>
2023-06-10 17:10:05 +0900

    생각나서 댓글 추가

--- src/pages/Post.js
+++ src/pages/Post.js
@@ -1,77 +1,101 @@
-// import { useState, useEffect } from "react";
-// import { useParams } from "react-router-dom";
-// import { doc, getDoc } from "firebase/firestore";
-// import { db } from "../firebase";
-
-// const Post = () => {
-//   const { id } = useParams();
-//   const [post, setPost] = useState(null);
-
-//   useEffect(() => {
-//     const fetchPost = async () => {
-//       const docRef = doc(db, "posts", id);
-//       const docSnap = await getDoc(docRef);
-//       if (docSnap.exists()) {
-//         setPost({ id: docSnap.id, ...docSnap.data() });
-//       } else {
-//         console.log("No such document!");
-//       }
-//     };
-
-//     fetchPost();
-//   }, [id]);
-
-//   if (!post) {
-//     return <div>Loading...</div>;
-//   }
-
-//   return (
-//     <div>
-//       <h1>{post.title}</h1>
-//       <p>{post.content}</p>
-//     </div>
-//   );
-// };
-
-// export default Post;
-
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { db } from "../firebase";
-import { doc, getDoc } from "firebase/firestore";
+import {
+  doc,
+  getDoc,
+  collection,
+  addDoc,
+  onSnapshot,
+  query,
+  where,
+} from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
-function PostDetail() {
+function PostDetail({ match }) {
   const { id } = useParams();
   const [post, setPost] = useState(null);
+  const [comments, setComments] = useState([]);
+  const [commentText, setCommentText] = useState("");
 
   useEffect(() => {
-    const fetchPost = async () => {
-      try {
-        const postRef = doc(db, "posts", id);
-        const docSnapshot = await getDoc(postRef);
-        if (docSnapshot.exists()) {
-          setPost({ id: docSnapshot.id, ...docSnapshot.data() });
-        } else {
-          console.log("해당하는 글을 찾을 수 없습니다.");
-        }
-      } catch (error) {
-        console.log(error);
-      }
-    };
+    const postId = id;
+    const postRef = doc(db, "posts", postId);
 
-    fetchPost();
+    const unsubscribePost = onSnapshot(postRef, (doc) => {
+      if (doc.exists()) {
+        setPost({ id: doc.id, ...doc.data() });
+      } else {
+        setPost(null);
+      }
+    });
+
+    const commentsRef = collection(db, "comments");
+    const postCommentsQuery = query(commentsRef, where("postId", "==", postId));
+
+    const unsubscribeComments = onSnapshot(postCommentsQuery, (snapshot) => {
+      const commentsData = snapshot.docs.map((doc) => doc.data());
+      setComments(commentsData);
+    });
+
+    return () => {
+      unsubscribePost();
+      unsubscribeComments();
+    };
   }, [id]);
 
+  const handleCommentSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const currentUser = getAuth().currentUser;
+      if (!currentUser) {
+        throw new Error("로그인되어 있지 않습니다.");
+      }
+
+      const commentData = {
+        postId: id,
+        content: commentText,
+        author: currentUser.displayName,
+      };
+
+      await addDoc(collection(db, "comments"), commentData);
+
+      setCommentText("");
+    } catch (error) {
+      console.error("댓글 등록 중 오류가 발생했습니다.", error);
+    }
+  };
+
   if (!post) {
-    return <div>Loading...</div>;
+    return <div>글을 불러오는 중입니다...</div>;
   }
 
   return (
     <div>
-      <h1>{post.title}</h1>
-      <p>글 작성자: {post.author}</p>
+      <h2>{post.title}</h2>
+      <p>작성자: {post.author}</p>
       <p>별점: {post.rating}</p>
       <p>{post.content}</p>
+
+      <h3>댓글</h3>
+      <ul>
+        {comments.map((comment, index) => (
+          <li key={index}>
+            <div>{comment.author}</div>
+            {comment.content}
+          </li>
+        ))}
+      </ul>
+
+      <form onSubmit={handleCommentSubmit}>
+        <textarea
+          placeholder="댓글을 작성하세요"
+          value={commentText}
+          onChange={(e) => setCommentText(e.target.value)}
+        ></textarea>
+        <button type="submit">댓글 등록</button>
+      </form>
     </div>
   );
 }
