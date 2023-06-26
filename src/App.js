f1cbd3b43284b5511f8cf835bad8c29c011b544e
Wickhye <jihyesim170@gmail.com>
2023-06-27 08:16:42 +0900

    헤더 분리

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import { useEffect, useState } from "react";
-import { fireStore, app, authService } from "./firebase";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 import "./App.css";
@@ -9,89 +7,14 @@ import GlobalStyle from "./style";
 import Main from "./pages/Main";
 import Post from "./pages/Post";
 import Write from "./pages/Write";
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import Header from "./components/Header";
 
 function App() {
-  const [user, setUser] = useState(null);
-  const handleGoogleLogin = () => {
-    const provider = new GoogleAuthProvider();
-    signInWithPopup(authService, provider)
-      .then((result) => {
-        const user = result.user;
-        setUser(user);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
-
-  const handleLogout = () => {
-    authService
-      .signOut()
-      .then(() => {
-        setUser(null);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
-
-  useEffect(() => {
-    console.log(fireStore);
-  });
-
-  const [activePage, setActivePage] = useState("recent");
-
-  useEffect(() => {
-    console.log(activePage, "???");
-  }, [activePage]);
-
   return (
     <BrowserRouter>
       <GlobalStyle />
 
-      <S.Header>
-        <S.HeaderTop>
-          <div>
-            {/* <img src="" alt="로고" /> */}
-            <S.LogoText>로고</S.LogoText>
-          </div>
-
-          {user ? (
-            <>
-              <S.User>
-                {/* <span>{user.displayName}</span> */}
-                <S.LoginButton onClick={handleLogout}>로그아웃</S.LoginButton>
-              </S.User>
-            </>
-          ) : (
-            <S.LoginButton onClick={handleGoogleLogin}>로그인</S.LoginButton>
-          )}
-        </S.HeaderTop>
-        <S.HeaderBottom>
-          <S.headerItem isActive={activePage === "recent"}>
-            <Link
-              to={`/`}
-              onClick={() => {
-                setActivePage("recent");
-              }}
-            >
-              최근
-            </Link>
-          </S.headerItem>
-          <S.headerItem isActive={activePage === "popular"}>
-            <Link
-              to={`/write`}
-              onClick={() => {
-                setActivePage("popular");
-              }}
-            >
-              주간 인기
-            </Link>
-          </S.headerItem>
-        </S.HeaderBottom>
-      </S.Header>
-
+      <Header />
       <Routes>
         <Route path="/" element={<Main />}></Route>
         <Route path="/write" element={<Write />}></Route>
---
+++ src/components/Header.js
@@ -0,0 +1,150 @@
+import { useEffect, useState } from "react";
+import { fireStore, app, authService } from "../firebase";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { styled } from "styled-components";
+import { Link } from "react-router-dom";
+
+function Header() {
+  const [user, setUser] = useState(null);
+  const handleGoogleLogin = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(authService, provider)
+      .then((result) => {
+        const user = result.user;
+        setUser(user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const handleLogout = () => {
+    authService
+      .signOut()
+      .then(() => {
+        setUser(null);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    console.log(fireStore);
+  });
+
+  const [activePage, setActivePage] = useState("recent");
+
+  useEffect(() => {
+    console.log(activePage, "???");
+  }, [activePage]);
+
+  return (
+    <HeaderBox>
+      <HeaderTop>
+        <div>
+          {/* <img src="" alt="로고" /> */}
+          <LogoText>로고</LogoText>
+        </div>
+
+        {user ? (
+          <>
+            <User>
+              {/* <span>{user.displayName}</span> */}
+              <LoginButton onClick={handleLogout}>로그아웃</LoginButton>
+            </User>
+          </>
+        ) : (
+          <LoginButton onClick={handleGoogleLogin}>로그인</LoginButton>
+        )}
+      </HeaderTop>
+      <HeaderBottom>
+        <HeaderItem isActive={activePage === "recent"}>
+          <Link
+            to={`/`}
+            onClick={() => {
+              setActivePage("recent");
+            }}
+          >
+            최근
+          </Link>
+        </HeaderItem>
+        <HeaderItem isActive={activePage === "popular"}>
+          <Link
+            to={`/write`}
+            onClick={() => {
+              setActivePage("popular");
+            }}
+          >
+            주간 인기
+          </Link>
+        </HeaderItem>
+      </HeaderBottom>
+    </HeaderBox>
+  );
+}
+
+export default Header;
+
+const HeaderBox = styled.header`
+  position: sticky;
+  top: 0;
+  background-color: white;
+`;
+
+const HeaderTop = styled.div`
+  height: 100px;
+
+  color: white;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  margin-bottom: 1rem;
+
+  margin: auto 20%;
+
+  font-family: "GmarketSansTTFMedium";
+`;
+
+const HeaderBottom = styled.div`
+  margin: auto 20%;
+  display: flex;
+  flex-direction: row;
+
+  font-family: "GmarketSansTTFMedium";
+
+  border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+`;
+
+const HeaderItem = styled.div`
+  padding: 15px 25px;
+  font-weight: 600;
+  font-size: 15px;
+  border-bottom: ${(props) => (props.isActive ? "2px solid #0b53e4" : "none")};
+`;
+
+const LogoText = styled.div`
+  font-size: 25px;
+  font-weight: 600;
+  color: black;
+`;
+
+const LoginButton = styled.button`
+  color: black;
+  border: none;
+  height: 50px;
+  font-size: 20px;
+  font-weight: 600;
+
+  background-color: inherit;
+  cursor: pointer;
+
+  font-family: "GmarketSansTTFMedium";
+`;
+
+const User = styled.div`
+  color: black;
+  font-size: 20px;
+  background-color: inherit;
+`;
